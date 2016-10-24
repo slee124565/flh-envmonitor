579993bcb85a26bd5060daa5472919bc67ab3ab9
FLH <service@flh.com.tw>
2016-10-24 13:17:55 +0800

    bug fix

--- config/fullscreen.sh
+++ config/fullscreen.sh
@@ -1,3 +1,5 @@
+sudo /var/FLH-Web/config/ip_auto_config
+
 # sudo -u pi epiphany-browser -a --profile ~/.config http://127.0.0.1/index.html --display=:0 &
 sudo -u pi epiphany-browser -a --profile ~/.config /var/FLH-Web/html/index.html --display=:0 &
 #sudo -u pi epiphany-browser -a --profile ~/.config http://localhost/index.php --display=:0 &
